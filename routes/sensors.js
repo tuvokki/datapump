faf59b6edc410d259f2ce459411e43c388696448
Wouter <tuvokki@gmail.com>
2014-10-10 12:10:05 +0200

    add some capabilities for a sensor

--- routes/sensors.js
+++ routes/sensors.js
@@ -1,30 +1,50 @@
-var express   = require('express'),
-    router    = express.Router(),
-    sensorReg = require('save')('sensor'),
-
+var express    = require('express'),
+    router     = express.Router(),
+    sensorReg  = require('save')('sensor'),
+    sensorSpec = require('save')('sensordata')
 
 _sensorlist = [{name: "test", id: 1, active: true},{name: "tosti", id: 2, active: true},{name: "taart", id: 3, active: false}]
 
 _sensorlist.forEach(function(item) { 
   sensorReg.create({ name: item.name, id: item.id, active: item.active }, function (error, user) {
-    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+    if (error) console.log("error saving data sensorReg: ", error)
+    else sensorSpec.create({sensor_id: item.id, capabilities: {temp: true, motion: false, video: false}})
   })
 })
 
 /* GET list   of sensors available. */
 router.get(  '/', function(req, res) {
   sensorReg.find({}, function (error, sensors) {
+    if (error) {
+      console.log("error fetching data sensorReg: ", error)
+      res.status(500).end()
+    }
     res.send(sensors)
   })
-  // sensorlist = {};
-  // sensorlist.sensors = _sensorlist
-  // res.send(sensorlist);
 });
 
 // GET /1 -> return the sensor with id 1
 router.get('/:id', function(req, res) {
     console.log(req.params.id);
-    res.send({sensor: _sensorlist[req.params.id - 1]});
+    //Reads a single entity with an idProperty of id
+    sensorReg.read(req.params.id, function (error, sensor) {
+    if (error) {
+      console.log("error fetching data sensorReg: ", error)
+      res.status(500).end()
+    }
+ 
+    if (sensor) {
+      //found a sensor, now get the specifications for it
+      sensorSpec.find({ 'sensor_id': sensor.id }, function(error, sensorspec) {
+        console.log("sensorspec", sensorspec)
+        
+        sensor.specs = sensorspec
+        res.send(sensor)
+      })
+    } else {
+      res.status(404).end()
+    }
+  })
 });
 
 module.exports = router;
