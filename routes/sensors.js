843a2067d125ba76ccab9c3f21ab765ead4d26b9
Wouter <tuvokki@gmail.com>
2014-10-10 12:07:51 +0200

    use save for the dataaccess

--- routes/sensors.js
+++ routes/sensors.js
@@ -1,13 +1,24 @@
-var express = require('express');
-var router = express.Router();
+var express   = require('express'),
+    router    = express.Router(),
+    sensorReg = require('save')('sensor'),
+
 
 _sensorlist = [{name: "test", id: 1, active: true},{name: "tosti", id: 2, active: true},{name: "taart", id: 3, active: false}]
 
-/* GET list of sensors available. */
-router.get('/', function(req, res) {
-  sensorlist = {};
-  sensorlist.sensors = _sensorlist
-  res.send(sensorlist);
+_sensorlist.forEach(function(item) { 
+  sensorReg.create({ name: item.name, id: item.id, active: item.active }, function (error, user) {
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+  })
+})
+
+/* GET list   of sensors available. */
+router.get(  '/', function(req, res) {
+  sensorReg.find({}, function (error, sensors) {
+    res.send(sensors)
+  })
+  // sensorlist = {};
+  // sensorlist.sensors = _sensorlist
+  // res.send(sensorlist);
 });
 
 // GET /1 -> return the sensor with id 1
