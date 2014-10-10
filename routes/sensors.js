83682c180751528cc48f060db20682b1056d18bc
Wouter <tuvokki@gmail.com>
2014-10-10 12:06:25 +0200

    return a single sensor

--- routes/sensors.js
+++ routes/sensors.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+_sensorlist = [{name: "test", id: 1, active: true},{name: "tosti", id: 2, active: true},{name: "taart", id: 3, active: false}]
+
 /* GET list of sensors available. */
 router.get('/', function(req, res) {
   sensorlist = {};
-  sensorlist.sensors = [{name: "test", id: 1, active: true},{name: "tosti", id: 2, active: true},{name: "taart", id: 3, active: false}]
+  sensorlist.sensors = _sensorlist
   res.send(sensorlist);
+});
+
+// GET /1 -> return the sensor with id 1
+router.get('/:id', function(req, res) {
+    console.log(req.params.id);
+    res.send({sensor: _sensorlist[req.params.id - 1]});
 });
 
 module.exports = router;
